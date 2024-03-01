5131c3f51cf8559a2583fce36c97f46629985046
AlexDanilov <adanilov@contractor.linuxfoundation.org>
2024-03-01 13:12:43 +0100

    Contributors counters: SnowFlake test cases

---
+++ src/k6/contributors-counters/sf/assert.ts
@@ -0,0 +1,8 @@
+import { IContributorsTotalSf } from '../../../types/contributors.type';
+import { RefinedResponse } from 'k6/http';
+
+export const contributorsCountersSfAssert = (res: RefinedResponse<any>): boolean => {
+  if (!res.body) return false;
+  const body = JSON.parse(res.body as string) as IContributorsTotalSf[];
+  return !!(body[0]['CONTRIBUTORS_COUNT']);
+}
