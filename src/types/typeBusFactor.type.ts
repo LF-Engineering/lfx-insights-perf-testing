e738e706ea32b2684264c2374a53d052d19848a3
AlexDanilov <adanilov@contractor.linuxfoundation.org>
2024-03-13 17:06:43 +0100

    TYPE_BUS_FACTOR thin cube test case Signed-off-by: Alex Danilov <adanilov@contractor.linuxfoundation.org>

--- src/types/typeBusFactor.type.ts
+++ src/types/typeBusFactor.type.ts
@@ -1,17 +1,3 @@
-/*export interface IContributorsTotalCube {
-  ['SnowContributions.project']: string;
-  ['SnowContributions.timestamp.month']: string;
-  ['SnowContributions.timestamp']: string;
-  ['SnowContributions.count_contributors']: string;
-}
-
-export interface IContributorsTotalCubeNew {
-  ['SnowSubSegments.project_slug']: string;
-  ['SnowActivities.timestamp.year']: string;
-  ['SnowActivities.timestamp']: string;
-  ['SnowActivities.metric_contributors']: string;
-}*/
-
 export interface ITypeBusFactorSf {
   ROW_NUMBER: number;
   USERNAME: string;
